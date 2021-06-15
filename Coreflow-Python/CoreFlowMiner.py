adb387eb7769512c29e6b305835dd08157e7e64d
Zinat <kzintas@umd.edu>
2021-06-15 10:59:41 -0400

    Fixed file references after move

--- Coreflow-Python/CoreFlowMiner.py
+++ Coreflow-Python/CoreFlowMiner.py
@@ -1,3 +1,5 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from Sequence import Sequence
 from TreeNode import TreeNode
 from OccurrencesMeanRankingFunction import OcccurrencesMeanRankingFunction
