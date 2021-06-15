abd737b2adba15e266c3df472c7e9707b1956b35
Zinat <kzintas@umd.edu>
2021-06-15 10:59:41 -0400

    Fixed file references after move

--- Coreflow-Python/OccurrencesMeanRankingFunction.py
+++ Coreflow-Python/OccurrencesMeanRankingFunction.py
@@ -1,3 +1,6 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from Pattern import Pattern
 class OcccurrencesMeanRankingFunction:
     def __init__(self):
--- Coreflow-Python/main.py
+++ Coreflow-Python/main.py
@@ -1,3 +1,5 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from EventStore import EventStore
 from Pattern import Pattern
 from CoreFlowMiner import CoreFlowMiner
