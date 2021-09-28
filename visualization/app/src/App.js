d51fc85f9bd80d62fc142e2b249a5bae03ae99b5
Kazi Tasnim Zinat <kzintas@umd.edu>
2021-09-28 16:59:36 -0400

    Added websute

---
+++ visualization/app/src/App.js
@@ -0,0 +1,12 @@
+import DropdownDataset from "./dropdownDataset";
+
+function App() {
+  return (
+    <div className="App">
+    Rima, Hello from  World!
+    < DropdownDataset />
+    </div>
+  );
+}
+
+export default App;
