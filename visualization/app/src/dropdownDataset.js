6bb8d7911e061e7e7eb3d672c7cfd247c60bf548
Kazi Tasnim Zinat <kzintas@umd.edu>
2021-09-30 01:06:56 -0400

    Dropdown detects state

--- visualization/app/src/dropdownDataset.js
+++ visualization/app/src/dropdownDataset.js
@@ -1,73 +1,64 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-import React, {useState} from "react";
-import { Dropdown, DropdownButton} from "react-bootstrap";
+import React, { useState } from "react";
+import { Dropdown, DropdownButton } from "react-bootstrap";
 
 function CreateSelectItems(props) {
   //console.log(props.value);
-  return <Dropdown.Item eventkey={props.value}>{props.value.substring(2)}</Dropdown.Item>;
-  // let items = [];
-  // for (let data of dataList) {
-  //     items.push(<Dropdown.Item eventkey={data} value={data}>{data}</Dropdown.Item>);
-  //     //here I will be creating my options dynamically based on
-  //     //what props are currently passed to the parent component
-  // }
-  // return items;
+  return <Dropdown.Item eventkey={props.value}>{props.value}</Dropdown.Item>;
 }
 
-// function onDropdownSelected(eventkey, evt) {
-//   //event.persist();
-//   //console.log("THE VAL", eventkey);
-//   console.log(evt.target);
-//   console.log(evt.target.innerText);
-//   setSelectedValue(evt.target.innerText);
-//   //here you will see the current selected value of the select input
-// }
-
 function DropdownDataset(props) {
-  const [selectedValue,setSelectedValue]=useState('');
-
-  const onDropdownSelected=(eventkey, evt)=>{
+  const [selectedValue, setSelectedValue] = useState("DND-Child");
+  
+  let dataMap = {};
+  const onDropdownSelected = (eventkey, evt) => {
     //event.persist();
     //console.log("THE VAL", eventkey);
     console.log(evt.target);
     console.log(evt.target.innerText);
     setSelectedValue(evt.target.innerText);
-    //here you will see the current selected value of the select input
-  }
+    console.log(selectedValue);
+    console.log(dataMap[String(selectedValue)]);
+
+  };
 
   const fileList = require.context(
     "../../../datasets/Outputs",
     false,
     /\.json$/
   );
-  //fileList.keys().forEach((key) => console.log(key));
-  //console.log(typeof fileList.keys()[0]);
   //Load json files
-  var dataMap = {};
   //TO_DO: Further nest the dictionary to have the format Dataset:Coreflow/SentenTree:File
   for (let file of fileList.keys()) {
-    let key = file.substring(0, 11);
+    let key = file.substring(2, 11);
     dataMap[key] = dataMap[key] || []; //initialize if not exists
     dataMap[key].push(require("../../../datasets/Outputs" + file.substring(1)));
   }
 
-  //console.log(dataMap);
-
-  const listItems = fileList
-    .keys()
-    .map((file) => <CreateSelectItems key={file} value={file} />);
+  // for (let dataset in dataMap){  
+  //   console.log(dataset);
+  //   console.log(dataMap[dataset]);
+  // }
+  console.log(typeof selectedValue);
+  const listItems = Object.keys(dataMap).map((file) => (
+    <CreateSelectItems key={file} value={file} />
+  ));
 
   return (
-    <DropdownButton
-      id="dropdown-basic-button"
-      title="Dropdown button"
-      onSelect={onDropdownSelected}
-    >
-      {listItems}
-      {/* {createSelectItems(fileList.keys())} */}
-    </DropdownButton>
+    <div>
+      <DropdownButton
+        id="dropdown-basic-button"
+        title="Dropdown button"
+        onSelect={onDropdownSelected}
+      >
+        {listItems}
+        {/* {createSelectItems(fileList.keys())} */}
+      </DropdownButton>
+      <h5>You selected  {selectedValue}</h5>
+      {/* <h5>You selected  {JSON.parse(dataMap[String(selectedValue)][0])}</h5> */}
+    </div>
     //   <Input type="select" onChange={this.onDropdownSelected} label="Multiple Select" multiple>
     //      {this.createSelectItems()}
     // </Input>
