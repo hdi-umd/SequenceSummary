45a6aa21b7e904d87dc7fab20ac887457e3fc779
Kazi Tasnim Zinat <kzintas@umd.edu>
2021-09-29 22:05:34 -0400

    populated dropdown

--- visualization/app/src/dropdownDataset.js
+++ visualization/app/src/dropdownDataset.js
@@ -1,76 +1,82 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-import React from "react";
-import { Dropdown, DropdownButton } from "react-bootstrap";
+import React, {useState} from "react";
+import { Dropdown, DropdownButton} from "react-bootstrap";
 
-//var fileSystem = require("file-system")
-
-const path = require("path");
-// const fs = require("fs");
-
-function DropdownDataset() {
-  // function importAll(r) {
-  //   return r.keys().map(r);
+function CreateSelectItems(props) {
+  //console.log(props.value);
+  return <Dropdown.Item eventkey={props.value}>{props.value.substring(2)}</Dropdown.Item>;
+  // let items = [];
+  // for (let data of dataList) {
+  //     items.push(<Dropdown.Item eventkey={data} value={data}>{data}</Dropdown.Item>);
+  //     //here I will be creating my options dynamically based on
+  //     //what props are currently passed to the parent component
   // }
-  // console.log(path.join(__dirname,"../../../"));
-  // console.log(path.resolve('../../../../../'));
-  // console.log(path.isAbsolute(__dirname));
-  // console.log(path.join(__dirname,"../../../"));
-  // console.log(__dirname);
-  const fileList = require.context("../../../datasets/Outputs", false, /\.json$/);
-  fileList.keys().forEach((key) => console.log(key));
-  console.log(typeof fileList.keys()[0]);
+  // return items;
+}
+
+// function onDropdownSelected(eventkey, evt) {
+//   //event.persist();
+//   //console.log("THE VAL", eventkey);
+//   console.log(evt.target);
+//   console.log(evt.target.innerText);
+//   setSelectedValue(evt.target.innerText);
+//   //here you will see the current selected value of the select input
+// }
+
+function DropdownDataset(props) {
+  const [selectedValue,setSelectedValue]=useState('');
+
+  const onDropdownSelected=(eventkey, evt)=>{
+    //event.persist();
+    //console.log("THE VAL", eventkey);
+    console.log(evt.target);
+    console.log(evt.target.innerText);
+    setSelectedValue(evt.target.innerText);
+    //here you will see the current selected value of the select input
+  }
+
+  const fileList = require.context(
+    "../../../datasets/Outputs",
+    false,
+    /\.json$/
+  );
+  //fileList.keys().forEach((key) => console.log(key));
+  //console.log(typeof fileList.keys()[0]);
   //Load json files
   var dataMap = {};
-  // var dataList = [];
-  // var dataListCoreFlow = [];
-  // var dataListSententree = [];
-
+  //TO_DO: Further nest the dictionary to have the format Dataset:Coreflow/SentenTree:File
   for (let file of fileList.keys()) {
-    //As there are redundant entries in context
     let key = file.substring(0, 11);
     dataMap[key] = dataMap[key] || []; //initialize if not exists
     dataMap[key].push(require("../../../datasets/Outputs" + file.substring(1)));
   }
 
-  // const data = require("../../../datasets/Outputs" +
-  //   fileList.keys()[0].substring(1));
-  console.log(dataMap);
+  //console.log(dataMap);
 
-  //joining path of directory
-  //const directoryPath = path.join(__dirname, "Documents");
-  //passsing directoryPath and callback function
-  // listReactFiles(__dirname).then(files => console.log(files))
+  const listItems = fileList
+    .keys()
+    .map((file) => <CreateSelectItems key={file} value={file} />);
 
-  // fs.recurse('./', ['*.*'],
-  //   function(filepath, relative, filename) {
-  //   if (filename) {
-  //  console.log("FILE"+filename);
-  //   } else {
-  //   console.log("FOLDER"+filepath);
-
-  //   }
-  // });
-  // let directoryPath = "/home/zinat/Documents/Research/code/datasets/Outputs";
-  // console.log(directoryPath);
-  // fs.readdir(directoryPath, function (err, files) {
-  //   //handling error
-  //   if (err) {
-  //     return console.log("Unable to scan directory: " + err);
-  //   }
-  //   //listing all files using forEach
-  //   files.forEach(function (file) {
-  //     // Do whatever you want to do with the file
-  //     console.log(file);
-  //   });
-  // });
   return (
-    <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-      <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+    <DropdownButton
+      id="dropdown-basic-button"
+      title="Dropdown button"
+      onSelect={onDropdownSelected}
+    >
+      {listItems}
+      {/* {createSelectItems(fileList.keys())} */}
     </DropdownButton>
+    //   <Input type="select" onChange={this.onDropdownSelected} label="Multiple Select" multiple>
+    //      {this.createSelectItems()}
+    // </Input>
+
+    // <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+    //   <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+    //   <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+    //   <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+    // </DropdownButton>
   );
 }
 
