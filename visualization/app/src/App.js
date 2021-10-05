68bc57e3c2c2f9e0bc38a8ed8f3796e458a5a991
Kazi Tasnim Zinat <kzintas@umd.edu>
2021-10-05 19:16:23 -0400

    slider first commit:

--- visualization/app/src/App.js
+++ visualization/app/src/App.js
@@ -1,38 +1,33 @@
 import DropdownDataset from "./dropdownDataset";
 import RenderVisualization from "./visualization";
+import VolumeSlider from "./sliderMinSupport";
 import React, { useState } from "react";
 import "./App.css";
 
-const cf = "coreflow", st = "sententree";
+const cf = "coreflow",
+  st = "sententree";
 
 function loadData() {
   let dataMap = {};
   const fileList = require.context("../public/assets", false, /\.json$/);
-  
-  let re = new RegExp('\\+(coreflow|sententree).*')
+
+  let re = new RegExp("\\+(coreflow|sententree).*");
   //Load json files
-  //TO_DO: Further nest the dictionary to have the format Dataset:Coreflow/SentenTree:File
   for (let file of fileList.keys()) {
-    //let key = file.substring(2, 11);
-    console.log(typeof file);
-    console.log(file.search(re));
-    let key = file.substring(2,).replace(re, "");
-    console.log(key);
-    console.log(file);
+    let key = file.substring(2).replace(re, "");
+
     dataMap[key] = dataMap[key] || {}; //initialize if not exists
     let minSup = file.substring(file.indexOf("_msp") + "+_msp".length);
     minSup = minSup.substring(0, minSup.indexOf(".json"));
-    console.log(minSup);
+
     if (file.indexOf("+coreflow") >= 0) {
       dataMap[key][cf] = dataMap[key][cf] || {};
       dataMap[key][cf][minSup] = "/assets" + file.substring(1);
-    };
+    }
     if (file.indexOf("+sententree") >= 0) {
       dataMap[key][st] = dataMap[key][st] || {};
       dataMap[key][st][minSup] = "/assets" + file.substring(1);
     }
-    //dataMap[key].push("/assets" + file.substring(1));
-    // dataMap[key].push(require("../../../datasets/Outputs" + file.substring(1)));
   }
   return dataMap;
 }
@@ -44,9 +39,7 @@ function App() {
 
   const [selectedValue, setSelectedValue] = useState(defaultVal);
   const selectedValueChange = (value) => {
-    // console.log(value);
     setSelectedValue(value);
-    // console.log(selectedValue);
   };
 
   console.log(defaultVal);
@@ -59,6 +52,7 @@ function App() {
           onSelectedValueChange={selectedValueChange}
           selectedVal={selectedValue}
         />
+        <VolumeSlider />
         <RenderVisualization
           dataSet={selectedValue}
           coreflowJson={data[selectedValue][cf][".05"]}
