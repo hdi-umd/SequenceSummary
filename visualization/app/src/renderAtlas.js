017ac8b54ee10bef3a51032d6717636a31244806
Kazi Tasnim Zinat <kzintas@umd.edu>
2021-10-06 20:13:33 -0400

    vertical svgs with border

--- visualization/app/src/renderAtlas.js
+++ visualization/app/src/renderAtlas.js
@@ -1,41 +1,34 @@
 import * as atlas from "atlas-vis";
 
-
 export async function renderTree(dataPath, renderer) {
   let scene = atlas.scene();
   console.log(dataPath);
   let data = await atlas.treejson(dataPath);
   console.log(data);
   // let data = "../../../datasets/Outputs/AAAsample_ed2coreflow_result.json"
-  let node = scene.mark("text", {
-    x: 100,
-    y: 100,
-    fontSize: "14px",
-    fontWeight: "bold",
-  });
+  let node = scene.mark("text", { x: 100, y: 100, fontSize: "14px" });
   let nodes = scene.repeat(node, data.nodeTable);
   scene.encode(node, { field: "event_attribute", channel: "text" });
-  nodes.layout = atlas.layout("tidytree", { width: 500, height: 300 });
+  nodes.layout = atlas.layout("tidytree", {
+    width: 300,
+    height: 500,
+    orientation: "vertical",
+  });
   scene.encode(node, {
     field: "average_index",
-    channel: "x",
-    rangeExtent: 600,
+    channel: "y",
+    rangeExtent: 400,
+    invertScale: true,
   });
-  scene.axis("x", "average_index", {
-    orientation: "bottom",
-    pathVisible: false,
-    tickVisible: false,
-  });
-  scene.gridlines("x", "average_index");
   let link = scene.mark("link", {
-    sourceAnchor: ["right", "middle"],
-    targetAnchor: ["left", "middle"],
-    strokeColor: "#888",
-    sourceOffset: [5, 0],
-    targetOffset: [-5, 0],
-    mode: "curveHorizontal",
+    sourceAnchor: ["center", "bottom"],
+    targetAnchor: ["center", "top"],
+    strokeColor: "#ddd",
+    sourceOffset: [0, 5],
+    targetOffset: [0, -5],
+    mode: "curveVertical",
   });
-  let links = scene.repeat(link, data.linkTable);
+  scene.repeat(link, data.linkTable);
   scene.encode(link, { channel: "source", field: "parent" });
   scene.encode(link, { channel: "target", field: "child" });
   scene.encode(link, {
@@ -43,10 +36,19 @@ export async function renderTree(dataPath, renderer) {
     field: "child.value",
     range: [0, 6],
   });
-  console.log(scene);
-  console.log(document.getElementById("svgElementCoreflow"));
-  //atlas.renderer("svg").render(scene, "svgElement");
-  
+  let lbl = scene.mark("text", {
+    x: 100,
+    y: 100,
+    fontSize: "14px",
+    fontWeight: "bold",
+    fillColor: "#006594",
+  });
+  scene.repeat(lbl, data.linkTable);
+  console.log(data.linkTable);
+  scene.encode(lbl, { field: "child.value", channel: "text" });
+  scene.affix(lbl, link, "x");
+  scene.affix(lbl, link, "y");
+
   renderer.clear();
   renderer.render(scene);
   //document.getElementById("svgElement").append("whatever");
@@ -56,26 +58,38 @@ export async function renderTree2(dataPath, renderer) {
   let scene = atlas.scene();
   let data = await atlas.graphjson(dataPath);
   let link = scene.mark("link", {
-    sourceAnchor: ["right", "middle"],
-    targetAnchor: ["left", "middle"],
-    sourceOffset: [5, 0],
-    targetOffset: [-5, 0],
-    mode: "curveHorizontal",
+    sourceAnchor: ["center", "bottom"],
+    targetAnchor: ["center", "top"],
+    sourceOffset: [0, 5],
+    targetOffset: [0, -5],
+    mode: "curveVertical",
+    strokeColor: "#ddd",
   });
   let links = scene.repeat(link, data.linkTable);
   let node = scene.mark("text", { x: 120, y: 120 });
   let nodes = scene.repeat(node, data.nodeTable);
-  nodes.layout = atlas.layout("force", { x: 200, y: 300, iterations: 300 });
+  nodes.layout = atlas.layout("force", {
+    x: 200,
+    y: 150,
+    iterations: 300,
+    repulsion: 90,
+    linkDistance: 10,
+  });
   scene.encode(node, {
     field: "average_index",
-    channel: "x",
-    rangeExtent: 900,
+    channel: "y",
+    rangeExtent: 400,
+    invertScale: true,
   });
   scene.encode(node, { field: "event_attribute", channel: "text" });
   scene.encode(link, { channel: "source", field: "source" });
   scene.encode(link, { channel: "target", field: "target" });
   scene.encode(link, { channel: "strokeWidth", field: "count", range: [0, 6] });
-  let linkWeight = scene.mark("text", { fillColor: "blue" });
+  let linkWeight = scene.mark("text", {
+    fillColor: "#006594",
+    fontSize: "14px",
+    fontWeight: "bold",
+  });
   scene.repeat(linkWeight, data.linkTable);
   scene.encode(linkWeight, { field: "count", channel: "text" });
   scene.affix(linkWeight, link, "x");
@@ -84,14 +98,3 @@ export async function renderTree2(dataPath, renderer) {
   renderer.render(scene);
   //atlas.renderer("svg","svgElement").render(scene, "svgElement");
 }
-function RenderVisualization(props) {
-  console.log(props.sententreeJson);
-  console.log(props.coreflowJson);
-  console.log(atlas);
-  let svgRendererCoreflow = atlas.renderer("svg", "svgElementCoreflow");
-  let svgRendererSententree = atlas.renderer("svg", "svgElementSententree");
-  renderTree(props.coreflowJson, svgRendererCoreflow);
-  renderTree2(props.sententreeJson, svgRendererSententree);
-  //return <> </>;
-}
-
