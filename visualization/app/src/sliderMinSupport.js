2076483a1a041a70814157fecb731b2d68c17ebe
Kazi Tasnim Zinat <kzintas@umd.edu>
2021-10-07 00:58:21 -0400

    working slider

--- visualization/app/src/sliderMinSupport.js
+++ visualization/app/src/sliderMinSupport.js
@@ -1,39 +1,31 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-import React, {useState, Component} from "react";
+import React, { useState, Component } from "react";
 
 // Using an ES6 transpiler like Babel
-import Slider from 'react-rangeslider'
+import Slider from "react-rangeslider";
 
 // To include the default styles
-import 'react-rangeslider/lib/index.css'
+import "react-rangeslider/lib/index.css";
 
 function SupportSlider(props) {
+  const handleOnChange = (value) => {
+    props.onSuppportChange(value.toFixed(2));
+  };
 
-    const [volume, setSliderVol] = useState(props.defaultSup);
-    
-  
-    const handleOnChange = (value) => {
-      
-      setSliderVol(value.toFixed(2));
-      console.log(volume);
-    }
-  
-    
-    return (
+  return (
     <Slider
-        min = {props.min}
-        max = {props.max}
-        step = {props.step}
-        value={volume}
-        orientation="horizontal"
-        tooltip={true} 
-        labels = {props.labels}
-        onChange={handleOnChange}
+      min={props.min}
+      max={props.max}
+      step={props.step}
+      value={parseFloat(props.selectedSup)}
+      orientation="horizontal"
+      tooltip={true}
+      labels={props.labels}
+      onChange={handleOnChange}
     />
-    )
-    
-  }
+  );
+}
 
 export default SupportSlider;
