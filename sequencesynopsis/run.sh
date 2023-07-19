92447478392f2c5d071987a17d2b9c305fba077d
Tracy Chen <ychen151@umd.edu>
2023-07-18 22:29:03 -0400

    add alpha and lambda args

---
+++ sequencesynopsis/run.sh
@@ -0,0 +1,1 @@
+python main.py --file "/Users/yuexichen/Desktop/code/collaborative_writing/processData/processed/sequenceDf_3-NNS.csv" --evttype 1 --startidx 1 --format "%m/%d/%y" --sep "," --local True --grpattr "Sequence" --attr "Event" --fileIdentifier "day3"
