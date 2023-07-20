aafb4582f613f4f998ddfae4dc12809cdc4b0b2c
Tracy Chen <ychen151@umd.edu>
2023-07-19 23:00:15 -0400

    add loop run

--- sequencesynopsis/run.sh
+++ sequencesynopsis/run.sh
@@ -1,2 +1,12 @@
-dayLang='5-NNS'
-python main.py --file "/Users/yuexichen/Desktop/code/collaborative_writing/processData/processed/sequenceDf_$dayLang.csv" --evttype 1 --startidx 1 --format "%m/%d/%y" --sep "," --local True --grpattr "Sequence" --attr "Event" --fileIdentifier "$dayLang"
+#!/bin/bash
+dayLang=('3-NNS' '4-NS' '5-NNS')
+metrics=('editDistance' 'Jaccard')
+action='action'
+
+for lang in "${dayLang[@]}"
+do
+    for metric in "${metrics[@]}"
+    do
+        python main.py --file "/Users/yuexichen/Desktop/code/collaborative_writing/processData/processed/${action}Df_${lang}_${metric}.csv" --evttype 1 --startidx 1 --format "%m/%d/%y" --sep "," --local True --grpattr "Sequence" --attr "Event" --fileIdentifier "${lang}_${action}_${metric}"
+    done
+done
