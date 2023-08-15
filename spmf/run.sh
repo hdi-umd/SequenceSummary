f6c029f29b7d9a1d84a253e38f9f37f5c3849335
Tracy Chen <ychen151@umd.edu>
2023-08-15 16:20:24 -0400

    update file paths

--- spmf/run.sh
+++ spmf/run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-identifiers=("author-NNS-day-1" "author-NNS-day-others" "author-NS-day-1" "author-NS-day-others")
-outF="output/author-NNS-day-1.csv"
-#inF="/Users/yuexichen/Desktop/code/collaborative_writing/processData/processed/ss_combinations/video_ssInput_author-NS-day-others.csv"
-inF="../coreflow_paper_test.csv"
-python main.py --file "${inF}" --evttype 1 --startidx 1 --format "%m/%d/%y" --sep "," --local True --grpattr "Sequence" --attr "Event" --output "${outF}"
+#slowList=("output/out_author-NNS-day-1_3_P0.csv" "output/out_author-NNS-day-1_2_P0.csv")
+slowList=()
+for inF in /Users/yuexichen/Desktop/code/collaborative_writing/processData/processed/VMSP/*.csv
+do
+    outF="output/out_$(basename "${inF}")"
+    echo ${outF}
+    if [[ "${slowList[*]}" != *"${outF}"* ]]
+    then
+        python main.py --file "${inF}" --evttype 1 --startidx 1 --format "%m/%d/%y" --sep "," --local True --grpattr "Sequence" --attr "Event" --output "${outF}"
+    fi
+done
