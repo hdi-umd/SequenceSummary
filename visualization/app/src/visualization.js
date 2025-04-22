cf8aa5ef4fda64ea7a737d70a11206a57e8c2ed3
kzintas <kzintas@umd.edu>
2025-04-22 15:52:35 -0400

    updated visualization main file

--- visualization/app/src/visualization.js
+++ visualization/app/src/visualization.js
@@ -1,33 +1,67 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-import * as atlas from "atlas-vis";
+import * as atlas from "mascot-vis"
 import { renderCoreFlow, renderSententree, renderSententree2, renderSeqSynopsis } from "./renderAtlas";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function RenderVisualization(props) {
-  console.log(props);
-  
-  const svgRendererCoreflow = atlas.renderer("svg", "svgElementCoreflow");
-  const svgRendererSentenTree = atlas.renderer("svg", "svgElementSentenTree");
-  const svgRendererSentenTree2 = atlas.renderer("svg", "svgElementSentenTree2");
-  const svgRendererSeqSynopsis = atlas.renderer("svg", "svgElementSeqSynopsis");
+  const [isLoading, setIsLoading] = useState(true);
 
-  renderCoreFlow(props.coreflowJson, svgRendererCoreflow);
-  renderSententree(props.sententreeJson, svgRendererSentenTree);
-  // renderSententree2(props.sententreeJson, svgRendererSentenTree2);
-  renderSeqSynopsis(props.seqsynopsisJson, svgRendererSeqSynopsis);
+  console.log(props);
+  useEffect(() => {
+    const renderVisualizations = async () => {
+      setIsLoading(true);
+      try {
+        // Wait for DOM elements to be ready
+        await new Promise(resolve => setTimeout(resolve, 900));
+    // const svgRendererCoreflow = atlas.renderer("svg", "svgElementCoreflow");
+    // const svgElements = document.querySelectorAll('.svgmined');
+    // svgElements.forEach(svg => {
+    //   svg.setAttribute('width', '400px');
+    //   svg.setAttribute('height', '600px');
+    // });
+
+    const svgRendererSentenTree = atlas.renderer("svg", "svgElementSentenTree");
+    // const svgRendererSentenTree2 = atlas.renderer("svg", "svgElementSentenTree2");
+    const svgRendererSeqSynopsis = atlas.renderer("svg", "svgElementSeqSynopsis");
+    const svgRendererCoreflow = atlas.renderer("svg", "svgElementCoreflow");
+
+    
+    // Initialize SVG dimensions
+    
+    // Validate data before rendering
+    if (props.coreflowJson && document.getElementById("svgElementCoreflow")) {
+      await renderCoreFlow(props.coreflowJson, svgRendererCoreflow);
+    }
+    if (props.sententreeJson && document.getElementById("svgElementSentenTree")) {
+      await renderSententree(props.sententreeJson, svgRendererSentenTree);
+    }
+
+    if (props.seqsynopsisJson && document.getElementById("svgElementSeqSynopsis")) {
+      await renderSeqSynopsis(props.seqsynopsisJson, svgRendererSeqSynopsis);
+    }
+
+  } catch (error) {
+    console.error("Error rendering visualizations:", error);
+  } finally {
+    setIsLoading(false);
+  }
+};
+
+renderVisualizations();
+},[props.sententreeJson, props.seqsynopsisJson, props.coreflowJson]);
   return (
-    <div id>
-      <svg id="svgElementSeqSynopsis" className="svgmined">
+    <div>
+      {/* <svg id="svgElementSeqSynopsis" className="svgmined">
           {" "}
-        </svg>
-        <svg id="svgElementCoreflow" className="svgmined">
+        </svg>  */}
+          {/* <svg id="svgElementCoreflow" className="svgmined">
           {" "}
-        </svg>
-        <svg id="svgElementSentenTree" className="svgmined">
+        </svg> */}
+         <svg id="svgElementSentenTree" className="svgmined">
           {" "}
-        </svg>
+        </svg> 
         {/* <svg id="svgElementSentenTree2" className="svgmined">
           {" "}
         </svg> */}
