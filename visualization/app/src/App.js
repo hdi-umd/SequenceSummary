06551e897ee139491436c4869d819999e55219e6
kzintas <kzintas@umd.edu>
2025-04-22 15:47:25 -0400

    updated APP.js file

--- visualization/app/src/App.js
+++ visualization/app/src/App.js
@@ -1,49 +1,66 @@
 import DropdownDataset from "./dropdownDataset";
 import RenderVisualization from "./visualization";
 import SupportSlider from "./sliderMinSupport";
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 import "./App.css";
 
 const cf = "coreflow",
   st = "sententree",
   ss = 'seqsynopsis';
 
+
 function loadData() {
   let dataMap = {};
   const fileList = require.context("../public/assets", false, /\.json$/);
   let minSupValues = {};
+  let filePath = "/assets";
 
   let re = new RegExp("\\+(coreflow|sententree|seqsynopsis).*");
   //Load json files
 
-  for (let file of fileList.keys()) {
-    let key = file.substring(2).replace(re, "");
+  try {
+    for (let file of fileList.keys()) {
+      let key = file.substring(2).replace(re, "");
 
-    dataMap[key] = dataMap[key] || {}; //initialize if not exists
-    let keyword = "_msp"
-    if (file.indexOf(keyword) === -1){
-      keyword = "_alpha"
+      dataMap[key] = dataMap[key] || {}; //initialize if not exists
+      let keyword = "_msp"
+      if (file.indexOf(keyword) === -1){
+        keyword = "_alpha"
+      }
+      let minSup = file.substring(file.indexOf(keyword) + keyword.length);
+      minSup = minSup.substring(0, minSup.indexOf(".json"));
+
+      if (!(minSup in minSupValues)) {
+        minSupValues[minSup] = minSup;
+      }
+
+      filePath = "/assets" + file.substring(1);
+      // console.log('fff', filePath);
+      if (file.indexOf("+coreflow") >= 0) {
+        dataMap[key][cf] = dataMap[key][cf] || {};
+        dataMap[key][cf][minSup] = filePath;
+      }
+      else if (file.indexOf("+sententree") >= 0) {
+        dataMap[key][st] = dataMap[key][st] || {};
+        dataMap[key][st][minSup] = filePath;
+      }
+      else if (file.indexOf("+seqsynopsis") >= 0) {
+        dataMap[key][ss] = dataMap[key][ss] || {};
+        dataMap[key][ss][minSup] = filePath;
+      }
+    } 
+    console.log("Data Map:", dataMap);
+    console.log("Support Values:", minSupValues);
+
+    return { dataMap: dataMap, minSup: minSupValues };
+  }catch (error) {
+    console.log(filePath);
+    console.error("Error loading data:", error);
+    
+    return { dataMap: {}, minSup: {} };
     }
-    let minSup = file.substring(file.indexOf(keyword) + keyword.length);
-    minSup = minSup.substring(0, minSup.indexOf(".json"));
-    if (!(minSup in minSupValues)) {
-      minSupValues[minSup] = minSup;
-    }
-    if (file.indexOf("+coreflow") >= 0) {
-      dataMap[key][cf] = dataMap[key][cf] || {};
-      dataMap[key][cf][minSup] = "/assets" + file.substring(1);
-    }
-    else if (file.indexOf("+sententree") >= 0) {
-      dataMap[key][st] = dataMap[key][st] || {};
-      dataMap[key][st][minSup] = "/assets" + file.substring(1);
-    }
-    else if (file.indexOf("+seqsynopsis") >= 0) {
-      dataMap[key][ss] = dataMap[key][ss] || {};
-      dataMap[key][ss][minSup] = "/assets" + file.substring(1);
-    }
-  }
-  console.log(minSupValues);
-  return { dataMap: dataMap, minSup: minSupValues };
+
+  
 }
 function App() {
   let dataDetails = loadData();
@@ -66,11 +83,42 @@ function App() {
   };
 
   const setSliderValueChange = (value) => {
-    setSliderValue(value);
-    console.log(sliderValue.substring(1));
+    setSliderValue(Number(value).toFixed(2)); // make sure it is two digits after the decimal point
+    console.log(value);
   };
 
   console.log(defaultVal);
+  const [isLoading, setIsLoading] = useState(false);
+  const [jsonData, setJsonData] = useState({
+    coreflow: null,
+    sententree: null,
+    seqsynopsis: null
+  });
+
+  useEffect(() => {
+    const loadJsonData = async () => {
+      setIsLoading(true);
+      try {
+        const [coreflowData, sententreeData, seqsynopsisData] = await Promise.all([
+          fetch(data[selectedValue][cf][sliderValue]).then(res => res.json()),
+          fetch(data[selectedValue][st][sliderValue]).then(res => res.json()),
+          fetch(data[selectedValue][ss][sliderValue]).then(res => res.json())
+        ]);
+
+        setJsonData({
+          coreflow: coreflowData,
+          sententree: sententreeData,
+          seqsynopsis: seqsynopsisData
+        });
+      } catch (error) {
+        console.error("Error loading JSON data:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadJsonData();
+  }, [selectedValue, sliderValue]);
   return (
     <div className="App">
       <div>
@@ -93,7 +141,7 @@ function App() {
           labels={support}
         />
       </div>
-      <div>
+      <div key={`${selectedValue}-${sliderValue}`}>
         <RenderVisualization
           dataSet={selectedValue}
           coreflowJson={data[selectedValue][cf][sliderValue]}
