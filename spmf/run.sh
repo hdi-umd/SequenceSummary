3f5ca6e80ad4e8e624bf77b735de6f9087f42499
kzintas <kzintas@umd.edu>
2025-04-22 20:00:32 -0400

    updated run file

--- spmf/run.sh
+++ spmf/run.sh
@@ -2,9 +2,9 @@
 #slowList=("output/out_author-NNS-day-1_3_P0.csv" "output/out_author-NNS-day-1_2_P0.csv")
 #slowList=("output/out_author-NNS-day-1_2_P0.csv" "output/out_author-NNS-day-1_3_P0.csv" "output/out_author-NNS-day-1_4_P0.csv" "output/out_author-NNS-day-1_4_P1.csv"\
 #"output/out_author-NNS-day-1_5_P0.csv" "output/out_author-NNS-day-1_5_P1.csv" "output/out_author-NNS-day-1_6_P0.csv")
-#slowList=("output/out_author-NNS-day-1_2_P0.csv")
-for inF in /Users/yuexichen/Desktop/code/collaborative_writing/processData/processed/VMSP/*.csv
-#for inF in ./input/*.csv
+slowList=("")
+# for inF in /Users/yuexichen/Desktop/code/collaborative_writing/processData/processed/VMSP/*.csv
+for inF in ./input/*.csv
 do
     outF="output/out_$(basename "${inF}")"
     if [ -e "${outF}" ]
