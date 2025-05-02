0626cb1b0a16498439df0d66a21bd6fb47eb8883
kzintas <kzintas@umd.edu>
2025-05-01 21:50:04 -0400

    removed redundant package files

--- runningDatasetsviaRunAll.sh
+++ runningDatasetsviaRunAll.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #Sample Dataset from Coreflow Paper Test
-python RunAll.py --file "datasets/coreflow_sample/Sample_Dataset.csv" --evttype 1 --startidx 1 --format "%m/%d/%y" --sep "," --local True --grpattr "Category" --attr "Event"
+python RunAll.py --file "datasets/coreflow_sample/Sample_Dataset.csv" --evttype 1 --startidx 1 --format "%m/%d/%y" --sep "," --local True --grpattr "Sequence" --attr "Event"
