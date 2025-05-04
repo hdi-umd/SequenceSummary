475e352b3561002b11a1d30ec7ed9a0b2ffdd08e
kzintas <kzintas@umd.edu>
2025-05-04 04:06:31 -0400

    updated gitignore for package

--- sequencesynopsis/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python main.py --file "../Sample_Dataset.csv" --evttype 1 --startidx 0 --format "%m/%d/%Y" --sep "," --local True --attr "Event" --split "day"
--- spmf/run.sh
+++
@@ -1,1 +0,0 @@
-python main.py --file "../Sample_Dataset.csv" --evttype 1 --startidx 0 --format "%m/%d/%Y" --sep "," --local True --attr "Event" 
